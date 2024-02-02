926dbd2fc7d24f475226771d055cb9be69e077ae
Jurayevkh <jurayevkh@gmail.com>
2024-02-02 23:51:11 +0500

    Background Service added

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,4 +1,12 @@
+using Bot;
+using Telegram.Bot;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var token = builder.Configuration.GetValue("BotToken",string.Empty);
+
+builder.Services.AddSingleton(new TelegramBotClient(token));
+builder.Services.AddHostedService<StartingBotBackgroundService>();
 
 var app = builder.Build();
 
