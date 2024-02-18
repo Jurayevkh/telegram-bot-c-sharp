024fe96252b12912ca914cc4119246aa627f4c07
Jurayevkh <jurayevkh@gmail.com>
2024-02-19 01:39:36 +0500

    Background Service

---
+++ Bot/Services/BotBackgroundService.cs
@@ -0,0 +1,28 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+namespace Bot;
+
+public class BotBackgroundService : BackgroundService
+{
+    private readonly ILogger<BotBackgroundService> _logger;
+    private readonly TelegramBotClient _client;
+    private readonly IUpdateHandler _updatehandler;
+
+    public BotBackgroundService(ILogger<BotBackgroundService> logger, TelegramBotClient client, IUpdateHandler updatehandler)
+    {
+        _logger = logger;
+        _client = client;
+        _updatehandler = updatehandler;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var bot = await _client.GetMeAsync(stoppingToken);
+        _logger.LogInformation($"Bot successfully started. {bot.Username}");
+
+        _client.StartReceiving(
+            _updatehandler.HandleUpdateAsync,
+            _updatehandler.HandlePollingErrorAsync,
+            new ReceiverOptions() {ThrowPendingUpdates=true},stoppingToken);
+    }
+}
