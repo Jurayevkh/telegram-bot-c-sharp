3511d6f94c7d7a0c4be78b934ca6fbbc6deedb46
Jurayevkh <jurayevkh@gmail.com>
2024-02-19 01:39:26 +0500

    Handler for messages

---
+++ Bot/Services/Handlers/HandlerMessageAsync.cs
@@ -0,0 +1,14 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Bot.Services;
+
+public partial class BotUpdateHandler
+{
+    public async Task HandlerMessageAsync(ITelegramBotClient botClient, Message? message, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(message);
+        var user = message.From;
+        _logger.LogInformation($"From {user.FirstName} was received new message");
+    }
+}
