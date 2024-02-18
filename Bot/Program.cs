c7420f172e45a3618ab94344e6a33c6ec574b6c4
Jurayevkh <jurayevkh@gmail.com>
2024-02-19 01:39:46 +0500

    Update handler

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,12 +1,15 @@
 using Bot;
+using Bot.Services;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var token = builder.Configuration.GetValue("BotToken",string.Empty);
 
-builder.Services.AddSingleton(new TelegramBotClient(token));
-builder.Services.AddHostedService<StartingBotBackgroundService>();
+builder.Services.AddSingleton(p=>new TelegramBotClient(token));
+builder.Services.AddSingleton<IUpdateHandler,BotUpdateHandler>();
+builder.Services.AddHostedService<BotBackgroundService>();
 
 var app = builder.Build();
 
---
+++ Bot/Services/BotUpdateHandler.cs
@@ -0,0 +1,48 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Bot.Services;
+
+public partial class BotUpdateHandler : IUpdateHandler
+{
+    private readonly ILogger<BotUpdateHandler> _logger;
+
+    public BotUpdateHandler(ILogger<BotUpdateHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Our bot has a new error sir :) : {exception.Message}");
+        return Task.CompletedTask;
+    }
+
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var handler = update.Type switch
+        {
+            UpdateType.Message => HandlerMessageAsync(botClient, update.Message, cancellationToken),
+            UpdateType.EditedMessage => EditedMessageHandlerAsync(botClient, update.Message, cancellationToken),
+            _ => UnknownUpdateHandler(botClient, update.Message, cancellationToken)
+        };
+        try
+        {
+        await handler;
+        }
+        catch(Exception e)
+        {
+            await HandlePollingErrorAsync(botClient,e,cancellationToken);
+        }
+    }
+
+    private Task UnknownUpdateHandler(ITelegramBotClient botClient, Message? message, CancellationToken cancellationToken)
+    {
+        var user = message.From;
+        _logger.LogInformation($"The user {user.FirstName} was send some unknown message!");
+        return Task.CompletedTask;
+    }
+}
+
