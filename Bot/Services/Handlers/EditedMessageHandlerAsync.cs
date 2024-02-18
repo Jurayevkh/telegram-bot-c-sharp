0f432ea13a9c1a89edcf28dcfa2a43b6731b36e4
Jurayevkh <jurayevkh@gmail.com>
2024-02-19 01:39:08 +0500

    Handler For edit messages

---
+++ Bot/Services/Handlers/EditedMessageHandlerAsync.cs
@@ -0,0 +1,16 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Bot.Services;
+
+public partial class BotUpdateHandler
+{
+    private async Task EditedMessageHandlerAsync(ITelegramBotClient botClient, Message? message, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(message);
+
+        var user = message.From;
+        _logger.LogInformation($"{user.FirstName} was edited their:\"{message.Text}\" message");
+    }
+}
+
