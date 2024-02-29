f6e1480184e561899c7a797ffae5c12e35f46f99
Jurayevkh <jurayevkh@gmail.com>
2024-03-01 00:05:41 +0500

    Other

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,15 +1,21 @@
 using Bot;
+using Bot.Data;
 using Bot.Services;
+using Microsoft.EntityFrameworkCore;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var token = builder.Configuration.GetValue("BotToken",string.Empty);
+builder.Services.AddDbContext<BotDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddSingleton(p=>new TelegramBotClient(token));
-builder.Services.AddSingleton<IUpdateHandler,BotUpdateHandler>();
+var token = builder.Configuration.GetValue("BotToken", string.Empty);
+
+builder.Services.AddSingleton(p => new TelegramBotClient(token!));
+builder.Services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
 builder.Services.AddHostedService<BotBackgroundService>();
+
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddLocalization();
 
@@ -17,11 +23,10 @@ var app = builder.Build();
 
 var supportedCultures = new[] { "uz-Uz", "en-US", "ru-Ru" };
 var localizationOptions = new RequestLocalizationOptions()
-    .SetDefaultCulture(supportedCultures[0])
+    .SetDefaultCulture(supportedCultures[1])
     .AddSupportedCultures(supportedCultures)
     .AddSupportedUICultures(supportedCultures);
 
 app.UseRequestLocalization(localizationOptions);
 
 app.Run();
-
