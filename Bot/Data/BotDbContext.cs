65038ab1026c8274f280685158224977ea4ef6ec
Jurayevkh <jurayevkh@gmail.com>
2024-03-01 00:02:35 +0500

    DbContext

---
+++ Bot/Data/BotDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bot.Entity;
+using Bot.Configuration;
+
+namespace Bot.Data;
+
+public class BotDbContext : DbContext
+{
+	public BotDbContext(DbContextOptions<BotDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<User> Users;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+		modelBuilder.ApplyConfiguration(new UserConfiguration());
+    }
+}
