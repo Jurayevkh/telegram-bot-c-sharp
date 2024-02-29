90972464da876d92f246e7cff40ec0ba29941258
Jurayevkh <jurayevkh@gmail.com>
2024-03-01 00:02:56 +0500

    Configuration

---
+++ Bot/Configuration/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Bot.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bot.Configuration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(user => user.UserId);
+        builder.HasIndex(user => user.ChatId);
+        builder.Property(user=>user.FirstName).HasMaxLength(255);
+        builder.Property(user => user.LastName).HasMaxLength(255);
+        builder.Property(user=>user.LanguageCode).HasMaxLength(3).IsRequired();
+    }
+}
+
